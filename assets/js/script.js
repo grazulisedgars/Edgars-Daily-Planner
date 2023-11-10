55de8fcecf0e0a0cf92c45b91df954f8f85f73fb
grazulisedgars <grazulisedgars@gmail.com>
2023-11-10 21:23:53 +0200

    Adds colorCode function with TA help

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,16 +17,17 @@ currentDay()
         for (var i = 9; i <= 17; i++) {
             var timeblock = $("<div>");
             timeblock.addClass("time-block row hour")
+            timeblock.attr("id", "hour-" + i);
             if (i <12) {
-                timeblock.text(i + "AM");
+                timeblock.html('<div class = "col-2 col-md-1 hour text-center py-3">'+ (i) + "AM" + '</div>');
             } 
              else if(i === 12)
             {
-             timeblock.text (i + "PM");
+             timeblock.html('<div class = "col-2 col-md-1 hour text-center py-3">'+ (i) + "PM" + '</div>');
             }
             //  This part provided with ask BCS Learning Assistant
             else {
-                timeblock.text ((i-12) + "PM");
+                timeblock.html('<div class = "col-2 col-md-1 hour text-center py-3">'+ (i-12) + "PM" + '</div>');
             }
             //-----------------------------------------------------
             timeblock.append(createInputField());
@@ -45,19 +46,42 @@ currentDay()
 function createInputField () {
     var inputField = $("<input>");
     inputField.attr("type", "text");
-    inputField.addClass("textarea")
+    inputField.addClass("col-8 col-md-10 description")
     return inputField;
 }
 
 function saveBtn () {
     var saveBtn = $("<button>");
-    saveBtn.addClass("saveBtn");
+    saveBtn.addClass("btn saveBtn col-2 col-md-1");
     saveBtn.text("Save");
     return saveBtn;
 }
 
 // Color-code each timeblock based on past, present and future when the timeblock is viewed
 
+function colorCode () {
+//  var currentHour =dayjs().hour();
+ var currentHour = 22
+
+
+ $(".time-block").each(function () {
+var rowHour = parseInt($(this).attr("id").split("-")[1]);
+
+if(rowHour < currentHour) {
+    $(this).addClass("past");
+} else if (rowHour === currentHour) {
+    $(this).removeClass("past");
+    $(this).addClass("present");
+} else { 
+    $(this).removeClass("past");
+    $(this).removeClass("present");
+    $(this).addClass("future");
+}
+
+ })
+}
+
+colorCode()
 
 // Allow user to enter an event when they click a timeblock (look at the example day1)
 
